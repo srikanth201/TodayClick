4c0be91e831c6dd9cf61bd28d7cb672022aa4fdc
srikanth201 <srikanth8494@gmail.com>
2020-12-12 17:11:37 +0530

    Second push

---
+++ jacks/src/main/java/jacks/Three.java
@@ -0,0 +1,10 @@
+package jacks;
+
+public class Three {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Threeee");
+	}
+
+}
---
+++ jacks/src/main/java/jacks/two.java
@@ -0,0 +1,13 @@
+package jacks;
+
+import org.testng.annotations.Test;
+
+public class two {
+	
+	@Test
+	public void qa()
+	{
+		System.out.println("Two");
+	}
+
+}
